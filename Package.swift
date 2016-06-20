aa45732ea19c3dd6c12805ae2b2c926890eee5c3
Kazuhiro Sakamoto <kazu620@gmail.com>
2016-06-20 03:14:49 +0000

    Use Forked  Kitura-net Client

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ import PackageDescription
 let package = Package(
     name: "Kitura",
         dependencies: [
-            .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 0, minor: 17),
+            .Package(url: "https://github.com/kazu0620/Kitura-net.git", majorVersion: 0, minor: 17),
             .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 8),
             .Package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine.git", majorVersion: 0, minor: 16)
         ]
